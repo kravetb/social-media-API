55b5d69df00a269f8296b0a47463c93c2191e1d4
Your_Name Your_Surname <your@email>
2024-03-21 20:08:20 +0200

    Create model by follow and following users

--- social_network/models.py
+++ social_network/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from social_media_service.settings import AUTH_USER_MODEL
 
 
 class Post(models.Model):
@@ -7,3 +8,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    following_user_id = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    create = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('user_id', 'following_user_id'),)
+        index_together = (('user_id', 'following_user_id'),)
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"{self.user_id} follows {self.following_user_id}"
