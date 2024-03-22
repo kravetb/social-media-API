d95384b07423047761ec25f1615e08af560b52f6
Your_Name Your_Surname <your@email>
2024-03-22 19:26:14 +0200

    Add validation to admin page when try create UserFollow with same data

--- social_network/models.py
+++ social_network/models.py
@@ -12,21 +12,25 @@ class Post(models.Model):
 
 class UserFollowing(models.Model):
     user_id = models.ForeignKey(
-        AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="following"
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following"
     )
     following_user_id = models.ForeignKey(
-        AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="followers"
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers"
     )
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = (('user_id', 'following_user_id'),)
-        index_together = (('user_id', 'following_user_id'),)
+        unique_together = (("user_id", "following_user_id"),)
+        index_together = (("user_id", "following_user_id"),)
         ordering = ["-created"]
+
+    def validation_id_folowing_and_id_followers(self):
+        if self.user_id == self.following_user_id:
+            raise ValueError("Can't be same user_id and following_user_id!")
+
+    def save(self, *args, **kwargs):
+        self.validation_id_folowing_and_id_followers()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user_id} follows {self.following_user_id}"
